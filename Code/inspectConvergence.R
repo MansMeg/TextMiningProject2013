2800117f95d4cd9340120fef0267c0db0291b6f5
Måns Magnusson <manma97@c-211-149.eduroam.liu.se>
2013-09-09 20:27:42 +0200

    Hejsan

--- Code/inspectConvergence.R
+++ Code/inspectConvergence.R
@@ -4,14 +4,20 @@ fileDF<-data.frame(k=as.numeric(str_extract(savedModels,pattern="[0-9]+")),
                    fileName=savedModels,stringsAsFactors=F)
 fileDF$restartNo[is.na(fileDF$restartNo)]<-0
 fileDF<-fileDF[order(fileDF$k,fileDF$restartNo,decreasing=T),]
-fileDF<-fileDF[!duplicated(fileDF$k),]
+#fileDF<-fileDF[!duplicated(fileDF$k),]
 fileDF<-fileDF[order(fileDF$k),]
+row.names(fileDF)<-NULL
 
-inspectNo<-1
+inspectNo<-2
+rm(mcmcListOfList,modelobjList)
 load(file.path("Models",fileDF[inspectNo,3]))
+modelobjList[[1]]
+length(mcmcListOfList)
+fileDF[inspectNo,3]
 plot(mcmcListOfList[[length(mcmcListOfList)]])
 gelman.diag(mcmcListOfList[[length(mcmcListOfList)]])
 
+tempMcmcList<-mcmc.list()
 for (i in 1:length(mcmcListOfList[[1]])){
   mcmcChain<-numeric(0)
   for (j in 1:length(mcmcListOfList)){
